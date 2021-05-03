cf35f6d2fba88896152476323bac42907cf35a83
GzuzVonNazareth187 <hotvange@gmail.com>
2021-05-03 14:43:33 +0200

    k

--- Spieler.java
+++ Spieler.java
@@ -1,13 +1,7 @@
 
-/**
- * Beschreiben Sie hier die Klasse Spieler.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class Spieler
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+   
     private int x;
 
     /**
@@ -15,19 +9,14 @@ public class Spieler
      */
     public Spieler()
     {
-        // Instanzvariable initialisieren
+        
         x = 0;
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
+    
     public int beispielMethode(int y)
     {
-        // tragen Sie hier den Code ein
+        
         return x + y;
     }
 }
